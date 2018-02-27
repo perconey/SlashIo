d3ff9bccba4a65da9191b1e535e024002a593698
Perconey <perconey@gmail.com>
2018-02-27 22:03:11 +0100

    Unit test project

---
+++ SlashIoTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SlashIoTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SlashIoTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("486c38bd-a9c5-488c-a4ad-909faa7b7b2c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SlashIoTests/SlashIoTestClass.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlashIoTests
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    class SlashIoTestClass
+    {
+        [TestMethod]
+        public void Test()
+        {
+
+        }
+
+    }
+}
