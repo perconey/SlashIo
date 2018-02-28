3996e86a944165938b9a02dc32bb833a01d119fe
Perconey <perconey@gmail.com>
2018-02-28 23:19:09 +0100

    Added route debugger

--- SlashIo/App_Start/RouteConfig.cs
+++ SlashIo/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace SlashIo
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+                name: "ClientDetails",
+                url: "Client-{id}",
+                defaults: new {Controller = "Page", action = "Details"}
+
+                );
         }
     }
 }
