4288f343af6b30d7641e5fcea2516eb3a867cb43
Perconey <perconey@gmail.com>
2018-03-01 23:42:30 +0100

    favi, resources

--- SlashIo/Controllers/HomeController.cs
+++ SlashIo/Controllers/HomeController.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace SlashIo.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public string Index()
         {
-            return View();
+            return RouteData.ToString();
         }
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
 
         public ActionResult About()
         {
