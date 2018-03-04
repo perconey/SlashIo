03ad6d357ec43ffdc83b2941826aac2450de9a75
Perconey <perconey@gmail.com>
2018-03-04 18:26:04 +0100

    Deleted default content

--- SlashIo/Controllers/HomeController.cs
+++ SlashIo/Controllers/HomeController.cs
@@ -4,28 +4,15 @@ namespace SlashIo.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        public ActionResult Index()
         {
-            return RouteData.ToString();
+            return View();
         }
 
         //public ActionResult Index()
         //{
         //    return View();
         //}
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+            
     }
 }
