c727996acf2803200fd42eeb606144c0231e96bc
Perconey <perconey@gmail.com>
2018-03-05 23:15:36 +0100

    routeconfig

--- SlashIo/App_Start/RouteConfig.cs
+++ SlashIo/App_Start/RouteConfig.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -12,7 +9,7 @@ namespace SlashIo
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapMvcAttributeRoutes();
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
