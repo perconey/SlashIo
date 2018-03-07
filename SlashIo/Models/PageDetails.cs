cb7ced34e488050d491137031e2bdbf05f3c0f75
Perconey <perconey@gmail.com>
2018-03-07 23:55:12 +0100

    added PageDetails

---
+++ SlashIo/Models/PageDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SlashIo.Models
+{
+    public class PageDetails
+    {
+        private string _currentPrefix;
+        private string -
+
+        public string CurrentPrefix { get => _currentPrefix; set => _currentPrefix = value; }
+    }
+}
