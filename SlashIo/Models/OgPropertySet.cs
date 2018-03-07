35ab4af9b9b6ec33c150be0a4d73c0d51f04bddc
Perconey <perconey@gmail.com>
2018-03-08 00:23:10 +0100

    created open graph property set

---
+++ SlashIo/Models/OgPropertySet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SlashIo.Models
+{
+    public class OgPropertySet
+    {
+        private string _ogurl;
+        private string _ogtype;
+        private string _ogtitle;
+        private string _ogDescription;
+        private string _ogImage;
+
+        public string Ogurl { get => _ogurl; set => _ogurl = value; }
+        public string Ogtype { get => _ogtype; set => _ogtype = value; }
+        public string Ogtitle { get => _ogtitle; set => _ogtitle = value; }
+        public string OgDescription { get => _ogDescription; set => _ogDescription = value; }
+        public string OgImage { get => _ogImage; set => _ogImage = value; }
+    }
+}
--- SlashIo/Models/PageDetails.cs
+++ SlashIo/Models/PageDetails.cs
@@ -8,8 +8,17 @@ namespace SlashIo.Models
     public class PageDetails
     {
         private string _currentPrefix;
-        private string -
+
+        private bool _imagesAreUrlProvided;
+
+        private OgPropertySet _propSet;
+
+        private List<string> _imageUrls;
+
 
         public string CurrentPrefix { get => _currentPrefix; set => _currentPrefix = value; }
+        public bool ImagesAreUrlProvided { get => _imagesAreUrlProvided; set => _imagesAreUrlProvided = value; }
+        public List<string> ImageUrls { get => _imageUrls; set => _imageUrls = value; }
+        public OgPropertySet PropSet { get => _propSet; set => _propSet = value; }
     }
 }
