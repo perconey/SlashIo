1a6384d6027382a51127e13ed3c8ed2b142012b4
Perconey <perconey@gmail.com>
2018-03-18 23:26:11 +0100

    Enabled migrations

---
+++ SlashIo/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace SlashIo.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SlashIo.DAL.OgPropertySetContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(SlashIo.DAL.OgPropertySetContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
